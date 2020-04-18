896352a22885daae89e4fce7a417c20ca5f85588
Girish <pgirisha77@gmail.com>
2020-04-18 15:03:01 +0530

    Small change to trigger docker build

--- src/main/java/com/example/todoserviceregistry/TodoServiceRegistryApplication.java
+++ src/main/java/com/example/todoserviceregistry/TodoServiceRegistryApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class TodoServiceRegistryApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(TodoServiceRegistryApplication.class, args);
     }
 }
